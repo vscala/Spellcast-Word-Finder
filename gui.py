6d8f0d3e49a08463beb56caec8c295069e221194
Vincent Scala <vincent2000nc@gmail.com>
2023-02-05 02:15:32 -0500

    updated gui to support hover, started work on multipliers

--- gui.py
+++ gui.py
@@ -30,13 +30,14 @@ class App:
 		xoff, yoff = 25, 25
 		for i in range(5):
 			for j in range(5):
-				self.lineInput += [tk.Entry(root, textvariable=self.vals[i][j], validate="key")]
+				self.lineInput += [tk.Entry(root, textvariable=self.vals[i][j], validate="key", highlightthickness=2)]
 				self.lineInput[-1]["borderwidth"] = "1px"
 				self.lineInput[-1]["font"] = tkFont.Font(family='Times',size=10)
 				self.lineInput[-1]["fg"] = "#333333"
 				self.lineInput[-1]["justify"] = "center"
 				self.lineInput[-1]['validatecommand'] = (self.lineInput[-1].register(on_validate), '%P', i, j)
 				self.lineInput[-1].place(x=xoff+j*32,y=yoff+i*32,width=32,height=32)
+				self.lineInput[-1].configure(highlightbackground="black", highlightcolor="black", font=('Roboto', 16))
 
 		for i in range(3):
 			self.labels += [tk.Label(root)]
@@ -55,17 +56,63 @@ class App:
 		self.button.place(x=xoff,y=yoff+160,width=160,height=25)
 		self.button["command"] = self.button_command
 
+
+	class lblHover:
+		def __init__(self, label, path, skipped, lineInput, vals, word):
+			self.label = label
+			self.path = path
+			self.skipped = skipped
+			self.skipset = set(skipped)
+			self.lineInput = lineInput
+			self.label.bind('<Enter>', lambda _ : self.hover())
+			self.label.bind('<Leave>', lambda _ : self.unhover())
+			self.vals = vals
+			self.temp = [[v.get().lower() for v in line] for line in self.vals]
+			self.word = word
+
+		def hover(self):
+			for (i, j), c in zip(self.path[::-1], self.word):
+				self.lineInput[i*5+j].configure(highlightbackground="blue", highlightcolor="blue", background="blue", font=('Roboto', 20, tk.font.BOLD), fg="white")
+				if (i, j) in self.skipset:
+					self.vals[i][j].set(c)
+
+			for i, j in self.skipped:
+				self.lineInput[i*5+j].configure(highlightbackground="red", highlightcolor="red", background="red", font=('Roboto', 20, tk.font.BOLD), fg="white")
+		
+		def unhover(self):
+			for i, j in self.path + self.skipped:
+				self.lineInput[i*5+j].configure(highlightbackground="black", highlightcolor="black", background="white", font=('Roboto', 16, tk.font.NORMAL), fg="black")
+				self.vals[i][j].set(self.temp[i][j])
+
+
 	def button_command(self):
 		board = [[v.get().lower() for v in line] for line in self.vals]
 		self.wb.setBoard(board)
 
-		wg = self.wb.generateWords()
-		wgS1 = self.wb.generateWords(1)	
-		wgS2 = self.wb.generateWords(2)
+		words = [
+			self.wb.bestWord(i) for i in range(3) # (best word, value, path, skipped)
+		]
+
+		wordLabelPrefix =[
+			"No swaps",
+			"One swap",
+			"Two swaps"
+		]
 		
-		self.labels[0]["text"] = f"No swaps: {next(wg)}"
-		self.labels[1]["text"] = f"One swap: {next(wgS1)}"
-		self.labels[2]["text"] = f"Two swaps: {next(wgS2)}"
+		for i, word in enumerate(words):
+			self.labels[i]["text"] = f"{wordLabelPrefix[i]}: {word[:2]}"
+			self.lblHover(self.labels[i], word[2], word[3], self.lineInput, self.vals, word[0])
+
+	'''
+		TODO implement
+			should allow user to input multiplier amount and cell
+			should update spellcast board to account for multiplier
+	'''
+	def addMultiplier(self, i, j, word=False):
+		self.wb.addMultiplier(i, j, 1)
+
+	def removeMultiplier(self, i, j):
+		self.wb.removeMultiplier(i, j)
 		
 
 if __name__ == "__main__":
--- spellcast.py
+++ spellcast.py
@@ -1,18 +1,31 @@
 # SpellCast
 
-from collections import defaultdict, Counter
-from itertools import chain
+from collections import Counter, defaultdict
+from functools import reduce
+from itertools import chain, product
 
-letter_values = [1, 4, 5, 3, 1, 5, 3, 4, 1, 7, 3, 3, 4, 2, 1, 4, 8, 2, 2, 2, 4, 5, 5, 7, 4, 8] 
-charset = set('abcdefghijklmnopqrstuvwxyz')
-value = lambda word : sum(letter_values[ord(c.lower()) - ord('a')] for c in word if c.lower() in charset) + (20 if len(word) > 6 else 0)
+LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+LETTERVALS = (1, 4, 5, 3, 1, 5, 3, 4, 1, 7, 3, 3, 4, 2, 1, 4, 8, 2, 2, 2, 4, 5, 5, 7, 4, 8)
 
 class WordBoard:
 	def __init__(self):
 		with open('words.txt') as f:
 			self.words = [word[:-1] for word in f.readlines()]
-		self.word_values = [(value(word), word) for word in self.words]
-		self.word_values.sort(reverse=True)
+
+	def recalculate(self):
+		maxGlobalMultiplier = max(self.wordMultipliers.values(), default=1)
+		maxCharMultiplier = defaultdict(lambda : 1)
+		for i, j in product(range(5), range(5)):
+			maxCharMultiplier[self.board[i][j]] = max(maxCharMultiplier[self.board[i][j]], maxGlobalMultiplier, self.letterMultipliers[(i, j)])
+
+		self.letterVals = [v * maxCharMultiplier[LETTERS[i]] for i, v in enumerate(LETTERVALS)]
+		self.boardValue = {}
+		for i, j in product(range(5), range(5)):
+			self.boardValue[(i, j)] = self.letterVals[ord(self.board[i][j].lower()) - ord('a')]
+		self.value = lambda word : sum(self.letterVals[ord(c.lower()) - ord('a')] for c in word if c.lower() in LETTERS) + (10 if len(word) > 6 else 0)
+		self.wordValues = [(self.value(word), word) for word in self.words]
+		self.wordValues.sort(reverse=True)
+
 		
 		
 	def setBoard(self, board):
@@ -20,6 +33,10 @@ class WordBoard:
 		self.n = len(board)
 		self.m = len(board[0])
 		self.totalCount = Counter(chain(*board))
+		
+		self.wordMultipliers = defaultdict(lambda : 1) # word multiplier at (i, j)
+		self.letterMultipliers = defaultdict(lambda : 1) # letter multiplier at (i, j)
+		self.recalculate()
 	
 	def precheck(self, word):
 		wCount = Counter(word)
@@ -27,27 +44,18 @@ class WordBoard:
 			if wCount[c] > self.totalCount[c]: return False
 		return True
 	
-	'''
-		word: the word to check if the board contains
-		skips: the number of letters in the word that can be skipped (TODO)
-		x, y: the position of a letter that must be included (None if no such letter exsist)
-		
-		return (the word path, value delta, and swap positions) # TODO
-	'''
-	def boardContains(self, word, skips=0, x=None, y=None) -> bool:
+	# returns (path, actualValue, skipped)
+	def boardContains(self, word, skips=0):
 		n, m = self.n, self.m
 		if not skips and not self.precheck(word):
-			return False
-		self.hitXY = (x == None)
-		self.skips = skips
+			return ([], 0, [])
 		
 		def backtrack(i, j, letters):
 			if not letters: return True
 			if self.board[i][j] != letters[0]: 
-				if self.skips and self.board[i][j] != '.': self.skips -= 1
+				if self.skips and self.board[i][j] != '.': 
+					self.skips -= 1
 				else: return False
-			if i == x and j == y:
-				self.hitXY = True
 			
 			temp, self.board[i][j] = self.board[i][j], "."
 			out = False
@@ -67,29 +75,65 @@ class WordBoard:
 				out = out or backtrack(i, j+1, letters[1:]) 		# 0, +1
 			if j > 0: 
 				out = out or backtrack(i, j-1, letters[1:])		# 0, -1
-			out = out and self.hitXY
 			
 			# reset board values
 			self.board[i][j] = temp
 			if self.board[i][j] != letters[0]: 
 				self.skips += 1
-			if i == x and j == y:
-				self.hitXY = False
+			
+			# if path found update path, skipped
+			if out: 
+				path.append((i, j))
+				if self.board[i][j] != letters[0]: 
+					skipped.append((i, j))
 			return out
 	   	
-		#Iterate over board
+		best = 0
+		out = ([], 0, [])
+		# Iterate over all starting positions
 		for i in range(n):
 			for j in range(m):
-				if self.board[i][j] == word: return True
+				self.skips = skips
+				path = []
+				skipped = []
+				# Singleton words
+				if self.board[i][j] == word: 
+					value = self.letterMultipliers[(i, j)] * self.wordMultipliers[(i, j)] * LETTERVALS[ord(word[0].lower()) - ord('a')]
+					if value > best:
+						out = ([(i, j)], value, [])
+						best = value
+				# Non-singleton words	
 				if self.board[i][j] == word[0]:
 					if backtrack(i, j, word):
-						return True   
+						wordMultiplier = reduce(lambda acc, cur : acc * self.wordMultipliers[cur], path, 1)
+						value = sum(self.letterMultipliers[x] * LETTERVALS[ord(word[ind].lower()) - ord('a')] for ind, x in enumerate(path[::-1])) * wordMultiplier
+						if value > best:
+							out = (path, value, skipped)
+							best = value
+		return out
 	
-	def generateWords(self, skips=0, x=None, y=None):
-		for value, word in self.word_values:
-			cur = self.boardContains(word, skips, x, y)
-			if cur:
-				yield (word, value, cur)
+	# returns (best word, value, path, skipped)
+	def bestWord(self, skips=0):
+		curBest = ("", 0, [], [])
+		curVal = 0
+		for _, word in self.wordValues:
+			path, actualValue, skipped = self.boardContains(word, skips)
+			if path and actualValue > curVal:
+				curVal = actualValue
+				curBest = (word, actualValue, path, skipped)
+		return curBest
+
+	def addMultiplier(self, i, j, m, word):
+		if word:
+			self.wordMultipliers[(i, j)] = m
+		else:
+			self.letterMultipliers[(i, j)] = m
+		self.recalculate()
+	
+	def removeMultiplier(self, i, j):
+		self.wordMultipliers[(i, j)] = 1
+		self.letterMultipliers[(i, j)] = 1
+		self.recalculate()
 
 if __name__ == "__main__":
 	# Read board and create wordboard
@@ -98,16 +142,16 @@ if __name__ == "__main__":
 	wb.setBoard(board)
 
 	# Find best words
-	wg = wb.generateWords()		# no swaps
-	wgS1 = wb.generateWords(1)	# one swap
-	wgS2 = wb.generateWords(2)	# two swaps
+	wg = wb.bestWord()		# no swaps
+	wgS1 = wb.bestWord(1)	# one swap
+	wgS2 = wb.bestWord(2)	# two swaps
 
 	#x, y = map(int, input().split())
 	#wgXY = wb.generateWords(x=x, y=y)
 
-	print("No swaps:", next(wg))
-	print("One swap:", next(wgS1))
-	print("Two swaps:", next(wgS2))
+	print("No swaps:", (wg))
+	print("One swap:", (wgS1))
+	print("Two swaps:", (wgS2))
 
 
 
